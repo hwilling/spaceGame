727a08834f778bc294fa142e11c329e4b05f1deb
Hunter WIllingham <hwilling@ycp.edu>
2017-11-05 12:41:39 -0500

    getter and mod functions for ship class

--- spaceGame/src/gameFiles/ship.java
+++ spaceGame/src/gameFiles/ship.java
@@ -2,7 +2,38 @@ package gameFiles;
 
 public class ship {
 	
-	ship(){
-		
+	String name;
+	int hpMax, hpCurrent, speed, movesRemaining, currentActions, maxActions;
+	
+	ship(String name){
+		this.name = name;
+	}
+	
+	int getHP(){
+		return hpCurrent;
+	}
+	void modHP(int modAmount){
+		hpCurrent += modAmount;
+		//make sure hp does not go over max amount
+		if(hpCurrent > hpMax){
+			hpCurrent = hpMax;
+		}
+		//TODO function to destroy ship if hp is less than 0 (switch this to controller?)
+	}
+	int getMovesRemaining(){
+		return movesRemaining;
+	}
+	void modMovesRemaining(int modAmount){
+		movesRemaining += modAmount;
+	}
+	int getCurrentActions(){
+		return currentActions;
+	}
+	void modCurrentActions(int modAmount){
+		currentActions += modAmount;
+	}
+	void turnReset(){
+		movesRemaining = speed;
+		currentActions = maxActions;
 	}
 }
