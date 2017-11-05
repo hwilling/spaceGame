c05d16276fbb475d4fa9fa484a2a7851e6b97e23
Hunter WIllingham <hwilling@ycp.edu>
2017-11-05 18:01:16 -0500

    junit class

---
+++ spaceGame/junit/gameTests/shipttests.java
@@ -0,0 +1,14 @@
+package gameTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class shipttests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
