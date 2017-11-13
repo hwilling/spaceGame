5c406e799fa8eadae44eded11a4d41d8b5801e1c
Liam <Liam@10.191.10.28>
2017-11-12 23:51:51 -0500

    More GUI progress.

--- spaceGame/src/gameFiles/gui.java
+++ spaceGame/src/gameFiles/gui.java
@@ -19,10 +19,12 @@ public class gui extends JPanel{
 	int y = 600;
 	int numPlayers = 0;
 	String name = null;
+	ArrayList<ship> player1Ships = new ArrayList<>();
+	ArrayList<ship> player2Ships = new ArrayList<>();
 	
 	private ship Ship = new ship(name);
 	private board Board = new board(x, y);
-	//private combatController combat = new combatController(numPlayers, null, null);
+	private combatController combat = new combatController(numPlayers, player1Ships, player2Ships);
 	private Timer timer;
 	
 	//test
@@ -37,8 +39,8 @@ public class gui extends JPanel{
 	
 	public gui() {
 		setBackground(Color.BLACK);
-		shipSprite = loadImage("gameFiles/shipSprite.png");
-		shipSprite2 = loadImage("gameFiles/shipSprite2Test.png");
+		//shipSprite = loadImage("gameFiles/shipSprite.png");
+		//shipSprite2 = loadImage("gameFiles/shipSprite2Test.png");
 		
 		setPreferredSize(new Dimension((int) Board.getXSize(), (int) Board.getYSize()));
 		
@@ -74,6 +76,20 @@ public class gui extends JPanel{
 			
 		};
 		addKeyListener(listen);
+		
+		InputMap im = getInputMap(WHEN_FOCUSED);
+        ActionMap am = getActionMap();
+
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_SPACE, 0), "onSpace");
+
+        am.put("onSpace", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Enter pressed
+            	greenShipX = 100;
+            	repaint();
+            }
+        });
 		
 		this.timer = new Timer(TIMER_INTERVAL, new ActionListener() {
 			@Override
@@ -149,22 +165,22 @@ public class gui extends JPanel{
 		
 	// Set the controller
 	public void setController(combatController combat) {
-		//this.combat = combat;
+		this.combat = combat;
 	}
 		
 	// Handle a paint event by drawing a visualization of the model data
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g); // paint background
-			
+		
 		// TODO: render the model
 		g.setColor(Color.GREEN);
-		//g.fillRect(greenShipX, greenShipY, 20, 20);
-		g.drawImage(shipSprite, greenShipX, greenShipY, 50, 50, this);
+		g.fillRect(greenShipX, greenShipY, 20, 20);
+		//g.drawImage(shipSprite, greenShipX, greenShipY, 50, 50, this);
 		
 		g.setColor(Color.RED);
-		g.drawImage(shipSprite2, redShipX, redShipY, 50, 50, this);
-		//g.fillOval(300, 50, 20, 20);
+		//g.drawImage(shipSprite2, redShipX, redShipY, 50, 50, this);
+		g.fillOval(redShipX, redShipY, 20, 20);
 
 	}
 	
@@ -177,15 +193,15 @@ public class gui extends JPanel{
 	            	int x = 0;
 	            	int y = 0;
 	            	int numPlayers = 0;
-	            	//ArrayList<ship> player1Ships = null;
-	            	//ArrayList<ship> player2Ships = null;
+	            	ArrayList<ship> player1Ships = new ArrayList<>();
+	            	ArrayList<ship> player2Ships = new ArrayList<>();
 	            	board Board = new board(x, y);
-	            	//combatController combat = new combatController(numPlayers, player1Ships, player2Ships);
+	            	combatController combat = new combatController(numPlayers, player1Ships, player2Ships);
 	            	
 	            	//combat.initModel(Board);
 	            	gui view = new gui();
 	            	view.setModel(Board);
-	            	//view.setController(combat);
+	            	view.setController(combat);
 
 	                JFrame frame = new JFrame();
 	                frame.setTitle("Space Game");
@@ -200,7 +216,7 @@ public class gui extends JPanel{
 	        });
 	    }
 	
-	// Load an image from an embedded resource.
+	/*// Load an image from an embedded resource.
 		private BufferedImage loadImage(String path) 
 		{
 			InputStream in = loadResource(path);
@@ -225,6 +241,6 @@ public class gui extends JPanel{
 			}
 			
 			return in;
-		}
+		}*/
 
 }
